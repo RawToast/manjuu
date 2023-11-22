b7b85ce61b84c690c312111da0242255d453f36f
James Morris <8013000+RawToast@users.noreply.github.com>
2023-11-22 20:49:02 +0000

    Add query provider to test

--- frontend/src/pages/search/__test__/searchbar.test.tsx
+++ frontend/src/pages/search/__test__/searchbar.test.tsx
@@ -3,9 +3,25 @@ import renderer from 'react-test-renderer'
 import { expect, test } from 'vitest'
 
 import { SearchBar } from '../searchbar'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 test('Renders', () => {
-  const underTest = <SearchBar />
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false
+      },
+      mutations: {
+        retry: false
+      }
+    }
+  })
+
+  const underTest = (
+    <QueryClientProvider client={queryClient}>
+      <SearchBar />
+    </QueryClientProvider>
+  )
 
   render(underTest)
   const childText = screen.getByPlaceholderText(/Type a command or search/i)
