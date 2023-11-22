4b56d3a4098ad73648ccd0b21149fe25aa048b9e
James Morris <8013000+RawToast@users.noreply.github.com>
2023-11-22 20:34:06 +0000

    enable cors

--- server/src/main/scala/manjuu/Server.scala
+++ server/src/main/scala/manjuu/Server.scala
@@ -8,9 +8,12 @@ import manjuu.services.util.AuthorityParser
 
 import cats.effect._
 import com.comcast.ip4s._
+import org.http4s.Method
 import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server._
 import org.http4s.implicits.uri
+import org.http4s.server.middleware.CORS
+import org.http4s.server.middleware.CORSConfig
 
 object Server extends IOApp:
 
@@ -21,16 +24,20 @@ object Server extends IOApp:
     .default[IO]
     .build
 
-  val authParser              = AuthorityParser.impl()
-  val fsaClient               = FSAClient.json(clientResource, uri"http://api.ratings.food.gov.uk")
-  val authService             = AuthorityService.impl(fsaClient, authParser)
-  val authorityController     = AuthorityController.impl[IO](authService, establishmentService)
+  val authParser          = AuthorityParser.impl()
+  val fsaClient           = FSAClient.json(clientResource, uri"http://api.ratings.food.gov.uk")
+  val authorityService    = AuthorityService.impl(fsaClient, authParser)
+  val authorityController = AuthorityController.impl[IO](authorityService, establishmentService)
+  val authorityHttpApp    = authorityController.routes.orNotFound
+  val corsEnabledApp      =
+    CORS.policy.withAllowOriginAll.withAllowMethodsIn(Set(Method.GET)).apply(authorityHttpApp)
+
   def run(args: List[String]) =
     EmberServerBuilder
       .default[IO]
       .withHost(ipv4"0.0.0.0")
       .withPort(port"8080")
-      .withHttpApp(authorityController.routes.orNotFound)
+      .withHttpApp(corsEnabledApp)
       .build
       .use(_ => IO.never)
       .as(ExitCode.Success)
