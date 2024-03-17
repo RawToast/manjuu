53eae3c7a86af5d6b4b67031152e40184452f831
RawToast <8013000+RawToast@users.noreply.github.com>
2024-03-17 18:01:32 +0900

    stub api

---
+++ stub-api/README.md
@@ -0,0 +1,16 @@
+# Manjuu Stub API
+
+This is a quick stub to develop locally or build tests against.
+
+This uses live like data for a select few authorities from the food hygiene service,
+but pagination has not been implemented.
+
+## Development
+
+To start the development server run:
+
+```bash
+bun dev
+```
+
+Open http://localhost:3000/ with your browser to see the result.
---
+++ stub-api/src/establishments.ts
@@ -0,0 +1,16 @@
+import { Map } from "immutable";
+import { amberValleyEstablishments } from "./data/amber-valley";
+import { aberdeenEstablishments } from "./data/aberdeen";
+import { amberdeenshireEstablishments } from "./data/aberdeenshire";
+import { angusEstablishments } from "./data/angus";
+import { adurEstablishments } from "./data/adur";
+import { angleseyEstablishments } from "./data/anglesey";
+
+export const establishmentsMap: Map<String, Object> = Map({
+  "48": amberValleyEstablishments,
+  "197": aberdeenEstablishments,
+  "198": amberdeenshireEstablishments,
+  "199": angusEstablishments,
+  "277": adurEstablishments,
+  "334": angleseyEstablishments,
+});
