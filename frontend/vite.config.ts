65caa3ba02ca393b407826414d671620847f9375
RawToast <8013000+RawToast@users.noreply.github.com>
2024-12-27 12:01:00 +0000

    Remove check

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,17 +4,12 @@ import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 import checker from 'vite-plugin-checker'
 
-// const isCi = process.env.NODE_ENV === 'ci'
-const isCi = false
-
 const plugins = [
   react(),
-  isCi
-    ? null
-    : checker({
-        typescript: true,
-        overlay: false
-      })
+  checker({
+    typescript: true,
+    overlay: false
+  })
 ]
 
 export default defineConfig({
