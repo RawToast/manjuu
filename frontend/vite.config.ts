f8c5cd89c42ce09b9c379db551de12afdde27e56
RawToast <8013000+RawToast@users.noreply.github.com>
2024-12-27 12:02:55 +0000

    Use overlay outside of CI

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,13 @@ import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 import checker from 'vite-plugin-checker'
 
+const isCi = process.env.NODE_ENV === 'ci'
+
 const plugins = [
   react(),
   checker({
     typescript: true,
-    overlay: false
+    overlay: isCi ? false : true
   })
 ]
 
